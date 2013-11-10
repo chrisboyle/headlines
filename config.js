c7e4ad32a6185b7ea155fcf120c228856763c499
Chris Boyle <chris@boyle.name>
2013-11-10 00:24:50 +0000

    Tidy: split config; add start command; better debug; ignore .swp.

---
+++ config.js
@@ -0,0 +1,9 @@
+module.exports = {
+	debug: true,
+	itemsPerFeed: 10,
+	feeds: {
+		'BBC News': 'http://feeds.bbci.co.uk/news/rss.xml',
+		'Sky News': 'http://news.sky.com/feeds/rss/home.xml'
+	}
+};
+
